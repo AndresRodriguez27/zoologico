c6ce80d840ff699184861ebd8816042ba12a1072
Jairo Andres Rodriguez <48247042+AndresRodriguez27@users.noreply.github.com>
2023-07-25 13:39:22 -0500

    Create import.sql

---
+++ zoo/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO rol (id, nombre) VALUES
+(1, 'ADMIN'),
+(2, 'EMPLEADO');
+
+INSERT INTO persona (idenficacion, nombre, celular ) VALUES
+(1, 'Andres', '321469');
+
+INSERT INTO usuario (id, email, password, rol_id ) VALUES
+(1, 'admin@gmial.com', 'admin', 1)
+
+INSERT INTO zona (id, nombre) VALUES
+(1, 'Acuario'),
+(2, 'Bosque');
+
