fabc540f0f09168b736590889ddd32f1e9b7fb24
Jairo Andres Rodriguez <48247042+AndresRodriguez27@users.noreply.github.com>
2023-07-28 23:24:41 -0500

    Update import.sql

--- zoo/src/main/resources/import.sql
+++ zoo/src/main/resources/import.sql
@@ -3,7 +3,7 @@ INSERT INTO rol (nombre) VALUES ('EMPLEADO');
 
 INSERT INTO persona (nombre, celular) VALUES ('Andres', '321469');
 
-INSERT INTO usuario (email, password, rol_id) VALUES ('admin@gmial.com', 'admin', 1);
+INSERT INTO usuario (email, password, rol_id) VALUES ('admin@gmial.com', '$2a$10$8gPui0HqOvOqJdjPLIi.mucS3ltR8Whu7NaKZo7vdz9rMADrYBjWG', 1);
 
 INSERT INTO zona (nombre) VALUES ('Desierto');
 INSERT INTO zona (nombre) VALUES ('Acuario');
