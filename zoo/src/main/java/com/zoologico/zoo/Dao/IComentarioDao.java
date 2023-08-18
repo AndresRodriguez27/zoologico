eb2c6ddbf3613b556f1d824c1d57a41b75cd8e46
Jairo Andres Rodriguez <48247042+AndresRodriguez27@users.noreply.github.com>
2023-08-18 13:43:36 -0500

    Delete zoo directory

--- zoo/src/main/java/com/zoologico/zoo/Dao/IComentarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.zoologico.zoo.Dao;
-
-import com.zoologico.zoo.entity.Comentario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IComentarioDao extends JpaRepository<Comentario, Long> {
-}
